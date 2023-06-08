228a455301e42558f1dbfc48ff779cee3251aaf5
Bradley <brad.holmes05@icloud.com>
2023-06-08 13:46:53 +0100

    create function

---
+++ 03_design_method/ex_3/lib/check_if_todo.py
@@ -0,0 +1,11 @@
+def check_if_todo(text: str) -> bool:
+    """
+    Parameters:
+        text: A string containing the text to check
+    Returns:
+        bool: Whether the text has '#TODO' 
+    Side Effects:
+        None
+    """
+    
+    pass
